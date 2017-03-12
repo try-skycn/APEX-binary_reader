3d1d913026e7a772cbbdb1af6a19fed6c6f74df5
Tianyao Chen <try.skycn@Gmail.com>
2017-03-12 09:50:48 +0800

    modify

--- makefile
+++ makefile
@@ -1,3 +1,5 @@
 .PHONY: all
 all:
 	python3 setup.py build_ext --inplace
+	rm reader.cpp
+	rm -rf build
--- reader.pyx
+++ reader.pyx
@@ -2,13 +2,13 @@ import numpy as np
 cimport numpy as np
 from libc.stdio cimport fopen, fclose, fread, fseek, FILE, SEEK_SET
 
-def read(filename, width, start, end):
+def read(filename, width, batch_size, start, end):
 	size = end - start
 	cdef FILE *f = fopen(filename, "rb")
-	fseek(f, sizeof(unsigned int) * width * start, SEEK_SET)
+	fseek(f, sizeof(unsigned int) * width * batch_size * start, SEEK_SET)
 	cdef unsigned int size_read
-	cdef np.ndarray[np.int32_t, ndim=1] buff = np.zeros((size * width), np.int32)
-	size_read = fread(<void*>buff.data, sizeof(unsigned int)*width, size, f)
+	cdef np.ndarray[np.int32_t, ndim=1] buff = np.zeros((size * batch_size * width), np.int32)
+	size_read = fread(<void*>buff.data, sizeof(unsigned int) * width * batch_size, size, f)
 	fclose(f)
-	return buff[:width*size_read].reshape((-1, width))
+	return buff.reshape((-1, batch_size, width))[:size_read]
 
